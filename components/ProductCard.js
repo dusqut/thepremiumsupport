e87f944b3c53eff6634175de0d858da8d4718915
Ravindu <dusqut@gmail.com>
2025-01-11 13:49:04 +0530

    product container size changed

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function ProductCard({ image, title, rating, description, price }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-1 gap-2 md:gap-4 border rounded-lg shadow-md p-2" id="AllProducts">
+    <div className="border rounded-lg shadow-md p-4 sm:max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg" id="AllProducts">
       <img src={image} alt={title} className="w-full h-57 object-cover rounded-md mb-4" />
       <h3 className="text-lg font-bold leading-tight">{title}</h3>
       <p className="text-yellow-500 text-lg leading-tight mt-1">{"★".repeat(rating)}</p>
--- components/ProductList.js
+++ components/ProductList.js
@@ -69,7 +69,7 @@ export default function ProductList() {
   ];
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 p-8">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 p-8 lg:p-24">
       {products.map((product, index) => (
         <ProductCard
           key={index}

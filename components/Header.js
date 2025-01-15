159f65513957502d94be5aa79eaab7d761387114
Ravindu <dusqut@gmail.com>
2025-01-15 17:43:12 +0530

    Meta Pixel Added for testing

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,7 @@
 // Header.js
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Header() {
   const [theme, setTheme] = useState("light");
@@ -25,6 +26,36 @@ export default function Header() {
 
   return (
     <header className="fixed top-0 left-0 w-full z-50 bg-white dark:bg-gray-900 shadow-md">
+        
+        <Head>
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;
+              n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1791479201615748'); 
+              fbq('track', 'PageView');
+            `,
+          }}
+        ></script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </Head>
+
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center py-4">
         {/* Logo and Title */}
         <div className="flex items-center">
